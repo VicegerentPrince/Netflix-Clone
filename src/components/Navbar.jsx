da9ddb32a40f1fd7d088e941cc0ef921a1af215a
VicegerentPrince <muneebshahzad319@gmail.com>
2025-07-11 12:04:52 +0500

    Few Tweaks to Navbar and Others

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className="navbar flex items-center justify-between w-full fixed left-0 top-0 px-10 py-8">
       <div className="navbar-left flex items-center gap-8">
-        <img src="/logo.png" alt="" className="h-8" />
+        <img src="/logo.png" className="logo h-8 cursor-pointer transition-all ease-in-out hover:scale-101 duration-300" alt="Logo"/>
         <ul className="flex gap-4 text-sm">
           <li className="cursor-pointer hover:scale-105 transition-transform">Home</li>
           <li className="cursor-pointer hover:scale-105 transition-transform">TV Shows</li>
@@ -21,11 +21,11 @@ const Navbar = () => {
       </div>
       <div className="navbar-right flex items-center gap-4">
         <MagnifyingGlassIcon className="h-8 hover:scale-107 ease-in-out transition-all" />
-        <p className="font-medium hover:scale-107 ease-in-out transition-all cursor-pointer">KIDS</p>
-        <BellIcon className="h-8 hover:scale-107 ease-in-out transition-all" />
-        <div className="flex items-center gap-0">
-          <UserCircleIcon className="h-8 text-blue-400 hover:text-blue-300 transition-all ease-in-out" />
-          <ChevronDownIcon className="h-6 hover:translate-y-1 ease-in-out transition-all" />
+        <p className=" hover:scale-107 ease-in-out transition-all cursor-pointer">KIDS</p>
+        <BellIcon className="h-7 hover:scale-107 ease-in-out transition-all" />
+        <div className="flex items-center gap-0 group">
+          <UserCircleIcon className="h-8 text-blue-400 group-hover:text-blue-300 transition-all ease-in-out" />
+          <ChevronDownIcon className="h-6 group-hover:translate-y-1 ease-in-out transition-all" />
         </div>
       </div>
     </div>
